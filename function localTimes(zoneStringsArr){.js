17db397dff7df5d08ba49c9ce23ad63bc6304fbe
Cem Simsek <cem@cemsimsek.com>
2016-04-15 11:35:57 +0300

    Delete function localTimes(zoneStringsArr){.js

--- function localTimes(zoneStringsArr){.js
+++
@@ -1,7 +0,0 @@
-function returnLocalTimeObj(zoneStringsArr){
-     var zoneStrings = {}
-     zoneStringsArr.forEach(function(zoneStr){
-        zoneStrings[""+zoneStr] = (new Date()).toLocaleTimeString("en-us",{timeZone:zoneStr});
-     });
-     return zoneStrings;
-}
